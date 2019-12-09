65dca4dd20d39dd00808c246de495eba2953548e
Alejandro Cruz Pérez <alex_crz97@hotmail.com>
2019-12-08 20:07:37 -0600

    Seccion 5

---
+++ 03-pipes/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  nombre:string = "Fernando";
+  nombre2:string = "alEjaNdrO cRuz pErez";
+
+  arreglo:number[] = [1,2,3,4,5,6,7,8,9,10];
+
+  PI:number = Math.PI;
+
+
+  a:number = 0.234;
+
+  salario:number = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "19"
+    }
+  }
+
+  valorDePromesa = new Promise ( ( resolve, reject ) => {
+    setTimeout( () => resolve('Llego la data!'), 5000);
+  });
+
+  fecha = new Date();
+
+  video:string = "v1i2C7yK5AQ";
+
+  activar:boolean = true;
+
+}
---
+++ 03-pipes/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID, NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import localEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+
+
+registerLocaleData(localEs);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
